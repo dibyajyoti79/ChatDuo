6528e3e7dad7b0b027e241f1eca316d78a787cd9
dibyajyoti79 <dibyajyoti274@gmail.com>
2024-05-31 20:41:40 +0530

    added build script

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
-
+import path from "path";
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -21,6 +21,14 @@ import userRoutes from "./routes/user.routes.js";
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/messages", messageRoutes);
 app.use("/api/v1/users", userRoutes);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 // error handler middleware
 app.use(errorMiddleware);
 export { app, server };
