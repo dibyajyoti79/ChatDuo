1eb6039d6acc92f5b2212caa690235ffd7f7365b
dibyajyoti79 <dibyajyoti274@gmail.com>
2024-05-31 19:43:18 +0530

    complete the api integration

---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const useGetConversations = () => {
+  const [loading, setLoading] = useState(false);
+  const [conversations, setConversations] = useState([]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("/api/v1/users");
+        const data = await res.json();
+        if (data.success === false) {
+          throw new Error(data.message);
+        }
+
+        setConversations(data.data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getConversations();
+  }, []);
+
+  return { loading, conversations };
+};
+export default useGetConversations;
---
+++ frontend/src/hooks/useSendMessage.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import useConversation from "../zustand/useConversation";
+import toast from "react-hot-toast";
+
+const useSendMessage = () => {
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+
+  const sendMessage = async (message) => {
+    setLoading(true);
+    try {
+      const res = await fetch(
+        `/api/v1/messages/send/${selectedConversation._id}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message }),
+        }
+      );
+      const data = await res.json();
+      if (data.success === false) {
+        throw new Error(data.message);
+      }
+
+      setMessages([...messages, data.data]);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { sendMessage, loading };
+};
+export default useSendMessage;
